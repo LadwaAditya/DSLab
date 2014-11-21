f9217c47e549f49b65262dea78b192455762b700
LadwaAditya <ladwa.aditya@gmail.com>
2014-11-21 14:19:24 +0530

    Initial Commit

---
+++ app/src/main/java/com/ladwa/aditya/dslab/MyActivity.java
@@ -0,0 +1,181 @@
+package com.ladwa.aditya.dslab;
+
+import android.content.SharedPreferences;
+import android.content.res.Configuration;
+import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentTransaction;
+import android.support.v4.view.GravityCompat;
+import android.support.v4.widget.DrawerLayout;
+import android.support.v7.app.ActionBarActivity;
+import android.support.v7.widget.Toolbar;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.view.View;
+import android.view.WindowManager;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
+
+import com.google.android.gms.ads.AdRequest;
+import com.google.android.gms.ads.AdView;
+import com.readystatesoftware.systembartint.SystemBarTintManager;
+
+
+public class MyActivity extends ActionBarActivity {
+
+    private String[] mList;
+    private DrawerLayout mDrawerLayout;
+    private ListView mDrawerList;
+    private android.support.v7.app.ActionBarDrawerToggle mDrawerToggle;
+    private SharedPreferences sp;
+    private int pos;
+    private static String PREF_POS = "pos";
+    final private static String FRAGMENT = "FragmentTag";
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_my);
+        Toolbar toolbar = (Toolbar) findViewById(R.id.my_awesome_toolbar);
+        setSupportActionBar(toolbar);
+
+        getWindow().addFlags(WindowManager.LayoutParams.FLAG_TRANSLUCENT_STATUS);
+
+        SystemBarTintManager tintManager = new SystemBarTintManager(this);
+        tintManager.setStatusBarTintEnabled(true);
+        tintManager.setStatusBarTintColor(getResources().getColor(R.color.primary));
+
+
+        AdView adView = (AdView) findViewById(R.id.adView);
+        AdRequest adRequest = new AdRequest.Builder().build();
+//        AdRequest adRequest = new AdRequest.Builder()
+//                .addTestDevice(AdRequest.DEVICE_ID_EMULATOR)
+//                .build();
+        adView.loadAd(adRequest);
+
+        mList = getResources().getStringArray(R.array.listLab);
+        mDrawerLayout = (DrawerLayout) findViewById(R.id.drawer_layout);
+        mDrawerList = (ListView) findViewById(R.id.left_drawer);
+
+        mDrawerList.setAdapter(new ArrayAdapter<String>(this, android.R.layout.simple_list_item_1, mList));
+        mDrawerList.setOnItemClickListener(new DrawerItemClickListener());
+
+        sp = getPreferences(MODE_PRIVATE);
+        pos = sp.getInt(PREF_POS, 0);
+
+
+        mDrawerToggle = new android.support.v7.app.ActionBarDrawerToggle(this, mDrawerLayout, R.string.drawer_open, R.string.drawer_close);
+
+
+        mDrawerLayout.setDrawerListener(mDrawerToggle);
+
+
+        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+        getSupportActionBar().setHomeButtonEnabled(true);
+
+        if (getSupportFragmentManager().findFragmentByTag(FRAGMENT) == null) {
+
+            Fragment fragment = new LabFragment();
+            Bundle data = new Bundle();
+            data.putInt("pos", pos);
+            fragment.setArguments(data);
+            getSupportFragmentManager().beginTransaction().add(R.id.content_frame, fragment, FRAGMENT).commit();
+
+        }
+
+    }
+
+
+    @Override
+    protected void onPostCreate(Bundle savedInstanceState) {
+        super.onPostCreate(savedInstanceState);
+        mDrawerToggle.syncState();
+    }
+
+    @Override
+    public void onConfigurationChanged(Configuration newConfig) {
+        super.onConfigurationChanged(newConfig);
+        mDrawerToggle.onConfigurationChanged(newConfig);
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.my, menu);
+        return true;
+    }
+
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+
+        if (mDrawerToggle.onOptionsItemSelected(item)) {
+            return true;
+        }
+
+        switch (item.getItemId()) {
+            case R.id.action_settings:
+                Fragment fragment = new SettingsFragment();
+                FragmentTransaction ft = getSupportFragmentManager().beginTransaction();
+                ft.replace(R.id.content_frame, fragment).setTransition(FragmentTransaction.TRANSIT_FRAGMENT_OPEN).addToBackStack(null).commit();
+                break;
+            case R.id.action_about:
+                fragment = new AboutFragment();
+                ft = getSupportFragmentManager().beginTransaction();
+                ft.replace(R.id.content_frame, fragment).setTransition(FragmentTransaction.TRANSIT_FRAGMENT_OPEN).addToBackStack(null).commit();
+                break;
+        }
+        return super.onOptionsItemSelected(item);
+    }
+
+    private class DrawerItemClickListener implements android.widget.AdapterView.OnItemClickListener {
+        @Override
+        public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+            selectItem(position);
+        }
+
+    }
+
+    @Override
+    public void onBackPressed() {
+
+
+        if (mDrawerLayout.isDrawerOpen(GravityCompat.START))
+            mDrawerLayout.closeDrawers();
+        else
+            super.onBackPressed();
+    }
+
+    private void selectItem(int position) {
+
+        mDrawerList.setItemChecked(position, true);
+        mDrawerLayout.closeDrawer(mDrawerList);
+
+        Bundle data = new Bundle();
+        data.putInt("pos", position);
+        pos = position;
+
+        SharedPreferences.Editor editor = sp.edit();
+        editor.putInt(PREF_POS, position);
+        editor.apply();
+
+        Fragment fragment = new LabFragment();
+        fragment.setArguments(data);
+        FragmentTransaction ft = getSupportFragmentManager().beginTransaction();
+
+        ft.replace(R.id.content_frame, fragment);
+        ft.addToBackStack(null);
+        ft.setTransition(FragmentTransaction.TRANSIT_FRAGMENT_OPEN);
+
+
+        ft.commit();
+
+
+    }
+
+
+}
